51f6c54a7af47c9e233d5f48d699a345b581cb17
Компьютер <@DESKTOP-RDMTMQN>
2024-12-01 14:49:07 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите значение x: ");
+double x = double.Parse(Console.ReadLine());
+double y = 1.8 * Math.Pow(x, 2) - Math.Sin(10 * x);
+Console.WriteLine($"При x = {x}, y = {y}");
